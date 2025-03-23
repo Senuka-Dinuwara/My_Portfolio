f8f56c09a0b2bebd89b5ffe1115669dd3ab05e39
Senuka Dinuwara <senudinubro@gmail.com>
2025-03-23 23:41:09 +0530

    update + content certificates & projects

--- js/project.js
+++ js/project.js
@@ -1,14 +1,28 @@
 const projects = [
-    
+    {   img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png",
+        title: "test1"
+    },
+    {   img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png",
+        title: "test2"
+    },
+    {   img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png",
+        title: "test3"
+    },
+    {   img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png",
+        title: "test4"
+    }
 ];
 
 function displayProjects() {
     const project_container = document.getElementById("projects-container");
     const Project = document.getElementById("project");
     if (projects.length == 0){
-        Project.innerHTML = `
-            <h3 class="noContent">Projects are currently being updated and will be available soon. Stay tuned for exciting updates!</h3>
+        const projectnoContent = document.createElement("section");
+        projectnoContent.classList.add("noContent");
+        projectnoContent.innerHTML = `
+            <h3>Projects are currently being updated and will be available soon. Stay tuned for exciting updates!</h3>
         `;
+        project_container.appendChild(projectnoContent);
     }else {
         projects.forEach(pro => {
             const projectElement = document.createElement("section");
@@ -21,8 +35,8 @@ function displayProjects() {
 
             Project.appendChild(projectElement);
         });
+        project_container.appendChild(Project);
     }
-    project_container.appendChild(Project);
 }
 
 window.onload = displayProjects;
