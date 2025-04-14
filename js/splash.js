c99dc36865ecf002c3ac46d622fe3d5a6f685fcf
Senuka Dinuwara <senudinubro@gmail.com>
2025-04-14 13:52:29 +0530

    update splash + resources + education begin

--- js/splash.js
+++ js/splash.js
@@ -1,15 +1,9 @@
-function Explore_Now(){
-    const button = document.getElementById('button_redirect');
-    const redirct_start = document.querySelector('.show_redirect');
-           
-    button.style.display = 'none';
-    redirct_start.style.display = 'block';
+const redirct_start = document.querySelector('.show_redirect');
 
-    setTimeout (() => {
-        const splash_screen = document.querySelector('.splash_screen');
-        const main_screen = document.querySelector('.main_screen');
+setTimeout (() => {
+    const splash_screen = document.querySelector('.splash_screen');
+    const main_screen = document.querySelector('.main_screen');
 
-        splash_screen.style.display = 'none';
-        main_screen.style.display = 'block';
-    },2000);
-}
+    splash_screen.style.display = 'none';
+    main_screen.style.display = 'block';
+},5000);
