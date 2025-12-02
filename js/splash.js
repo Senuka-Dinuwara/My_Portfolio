3bf5a27cd9ced7d375b20f8b5a8f1c09b5e1bb5b
Senuka Dinuwara <senudinubro@gmail.com>
2025-12-02 10:07:21 +0530

    new portfolio

--- js/splash.js
+++
@@ -1,9 +0,0 @@
-const redirct_start = document.querySelector('.show_redirect');
-
-setTimeout (() => {
-    const splash_screen = document.querySelector('.splash_screen');
-    const main_screen = document.querySelector('.main_screen');
-
-    splash_screen.style.display = 'none';
-    main_screen.style.display = 'block';
-},5000);
